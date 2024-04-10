48e2751d57e6051b9d523f37a95edf6ca993f934
dishasachdeva <127543878+DishaSachdeva-02@users.noreply.github.com>
2024-04-10 16:41:04 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,31 @@ import { RouterOutlet } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MessageComponent } from './message/message.component';
 import { RouterModule } from '@angular/router';
+import { movies } from './movies/movies';
+import { Observable,Subject ,debounceTime,distinctUntilChanged,switchMap} from 'rxjs';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { MoviesService } from './movies.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MoviesComponent,MessageComponent,RouterModule],
+  imports: [RouterOutlet,MoviesComponent,MessageComponent,RouterModule,AsyncPipe,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MY FAVOURITE MOVIES';
+  constructor(private movieservice:MoviesService){}
+  private searchTerms=new Subject<string>();
+  movies$!:Observable<movies[]>;
+  search(term:string){
+    this.searchTerms.next(term);
+  }
+  ngOnInit(){
+    this.movies$=this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term:string)=>this.movieservice.searchdata(term))
+    )
+    console.log(this.movies$);
+  }
 }
--- src/app/movies.service.ts
+++ src/app/movies.service.ts
@@ -5,6 +5,7 @@ import { Observable,of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
+import { catchError,tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,25 +15,56 @@ export class MoviesService {
   constructor(private messageservice:MessageService , private http:HttpClient) { }
   getMovies():Observable<movies[]>{
     // const message= of(FavMovies);
-    this.messageservice.add("All the movies are displayed....");
-    return this.http.get<movies[]>(this.url);
+    // this.messageservice.add("All the movies are displayed....");
+    return this.http.get<movies[]>(this.url).pipe(
+      tap(_=>this.log('fetched movies..')),
+     catchError(this.handleError<movies[]>('getMovies',[]))
+    );
     // return message;
   }
   getMov(id:number):Observable<movies>{
     // const movie=FavMovies.find(mov=>mov.id===id)!;
     const urlid=`${this.url}/${id}`
-    this.messageservice.add(`Selected movie is ${id}`);
-    return this.http.get<movies>(urlid)
+    // this.messageservice.add(`Selected movie is ${id}`);
+    return this.http.get<movies>(urlid).pipe(
+      tap(_=>this.log(`fetched movie detail whose id=${id}...`)),
+      catchError(this.handleError<movies>(`getMov id=${id}`))
+    );;
     // return of(movie);
   }
   updateMovie(movie:movies):Observable<any>{
     return this.http.put(this.url,movie,this.httpOptions);
   }
   add(movie:movies):Observable<movies>{
-    return this.http.post<movies>(this.url,movie,this.httpOptions)
+    return this.http.post<movies>(this.url,movie,this.httpOptions).pipe(
+      tap((newmovie:movies)=>this.log(`New character added with id=${newmovie.id}`)),
+      catchError(this.handleError<movies>('add'))
+    );
   }
   delete(id:number):Observable<movies>{
     const urlid=`${this.url}/${id}`
-    return this.http.delete<movies>(urlid,this.httpOptions);
+    return this.http.delete<movies>(urlid,this.httpOptions).pipe(
+      tap(_=>this.log(`deleted movie whose id=${id}`)),
+      catchError(this.handleError<movies>('delete'))
+    );
   }
+  searchdata(term:string):Observable<movies[]>{
+    if(!term.trim()){
+      return of([]);
+    }
+    return this.http.get<movies[]>(`${this.url}/?name=${term}`).pipe(
+      tap(_=>this.log(`searching data for ${term}`)),
+      catchError(this.handleError<any>('searchdata',[]))
+    );
+  }
+  private handleError<T>(operation='operation', result?:T){
+    return (error:any):Observable<T>=>{
+     this.log(`${operation} failed: ${error.message}`)
+     console.log(error);
+      return of(result as T)
+    }
+ }
+ private log(message:string){
+   this.messageservice.add(`${message}`);
+ }
 }
